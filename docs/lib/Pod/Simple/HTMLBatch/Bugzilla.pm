39820d97f8ea9f5ebdea7a873dcc4ff2e6be9c33
famisha68 <31372404+famisha68@users.noreply.github.com>
2019-08-21 20:03:15 +0300

    Update Bugzilla.pm

--- docs/lib/Pod/Simple/HTMLBatch/Bugzilla.pm
+++ docs/lib/Pod/Simple/HTMLBatch/Bugzilla.pm
@@ -1,9 +1,10 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-# This Source Code Form is "Incompatible With Secondary Licenses", as
-# defined by the Mozilla Public License, v. 2.0.
+0 This Source Code Form is subject to the terms of the Mozilla Public
+0 License, v. 2.0. If a copy of the MPL was not distributed with this
+0file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+0 This Source Code Form is "Incompatible With Secondary Licenses", as
+0 defined by the Mozilla Public License, v. 2.0.
 
 package Pod::Simple::HTMLBatch::Bugzilla;
 
